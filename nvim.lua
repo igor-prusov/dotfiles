b2ffbe8328f2b5e69ea4ef32783a35920593874a
Igor Prusov <prusovigor@gmail.com>
2024-02-04 17:42:50 +0300

    Add undotree

--- nvim.lua
+++ nvim.lua
@@ -1,6 +1,7 @@
 vim.keymap.set('','<F2>', '<cmd>NERDTreeToggle<CR>')
-vim.keymap.set('','<C-p>', '<cmd>FZF<CR>')
 vim.keymap.set('','<F3>', '<cmd>TagbarToggle<CR>')
+vim.keymap.set('','<F4>', '<cmd>UndotreeToggle<CR>')
+vim.keymap.set('','<C-p>', '<cmd>FZF<CR>')
 
 require('gitsigns').setup()
 
