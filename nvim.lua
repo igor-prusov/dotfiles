77eed8ca5a2d97fc3b928556d32f6bf323633f80
Igor Prusov <prusovigor@gmail.com>
2024-02-08 00:52:21 +0300

    Port some options from old config

--- nvim.lua
+++ nvim.lua
@@ -1,6 +1,7 @@
 vim.keymap.set('','<F2>', '<cmd>NERDTreeToggle<CR>')
 vim.keymap.set('','<F3>', '<cmd>TagbarToggle<CR>')
 vim.keymap.set('','<F4>', '<cmd>UndotreeToggle<CR>')
+vim.keymap.set('','<F12>', '<cmd>set invnumber<CR>')
 vim.keymap.set('','<C-p>', '<cmd>Telescope git_files<CR>')
 
 require('gitsigns').setup()
@@ -62,3 +63,6 @@ local telescope_builtin = require('telescope.builtin')
 vim.api.nvim_create_user_command('RG', function()
 	telescope_builtin.live_grep{}
 end, {})
+
+-- BufExplorer: Don't locate buffers by default
+vim.g.bufExplorerFindActive = 0
